a04e15171b725f6b7e5090d0ec2041af727ce4d6
YayaTheBehemoth <allanermansur@gmail.com>
2022-03-07 15:46:23 +0100

    first python test

---
+++ test.py
@@ -0,0 +1,27 @@
+#username = input("enter your name ")
+#print (username + " is the current username")
+#n1,n2 = input("enter 2 numbers ").split()
+#print ("n2 is ",n2)
+#print ("n1 is ",n1)
+#name = input("Enter your name ")
+#sleep = int(input("How much have you slept? "))
+#if sleep < 8:
+# print("tired? " + name)
+#else:
+# print("fresh? "  + name) 
+#for x in range (101):
+#    if x % 5 == 0:
+#        print ("gah damn")
+#    elif x % 3 == 0:
+#      print("ballers")
+#    elif x % 3 == 0 and x % 5 == 0:
+#        print ("real shit?")
+#    else:
+#        print(x)
+#else:
+#    print("we done")
+#number = int(input("Please enter a number: "))
+#for x in range(1,number+1):
+#    for y in range (1,11):
+#        print("{} X {} = {}".format(y,x,x*y))
+
