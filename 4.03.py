69df1b2a87d5365fe65cd9bd313c08cdbf5df52b
YayaTheBehemoth <allanermansur@gmail.com>
2022-03-09 10:02:35 +0100

    more python stuff

---
+++ 4.03.py
@@ -0,0 +1,18 @@
+
+
+
+class vehicle:
+  
+    def __init__(self,reg,color,year,make,model):
+       self.regno = str(reg) 
+       self.color = str(color)
+       self.year = year
+       self.make = make 
+       self.model = model
+a = vehicle("fuck","fuck","fuck","fuck","fuck")
+print(a.regno)
+print(a.color)
+print(a.year)
+print(a.make)
+print(a.model)
+
---
+++ 5.01.py
@@ -0,0 +1,7 @@
+class card:
+    def __init__(self,rank,suit):
+     self.rank = rank
+     self.suit = suit
+    
+     
+
